085824872e8fe716939d4a2372e01848ab840623
raminhrd <raminhr.work@gmail.com>
2026-01-28 22:04:14 +0100

    fix admin.py for booking

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from booking.models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "car",
+        "service",
+        "start_at",
+        "duration_minutes",
+        "status",
+        "created_at",
+    )
+    list_filter = ("status", "service", "created_at")
+    search_fields = (
+        "user__phone_number",
+        "car__pelak",
+        "car__vin",
+        "service__title",
+    )
+    autocomplete_fields = ("user", "car", "service")
+    ordering = ("-created_at",)
+    date_hierarchy = "start_at"
+    readonly_fields = ("created_at",)
