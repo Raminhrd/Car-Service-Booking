c78ee5943965cc2fe4b2ca97e0f1866db24e7ce7
raminhrd <raminhr.work@gmail.com>
2026-01-28 22:05:58 +0100

    fix admin.py for services 🐒

--- services/admin.py
+++ services/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from services.models import Service
 
-admin.register(Service)
-class RoleAdmin(admin.ModelAdmin):
-    list_display = ("id",
-            "title",
-            "service_type",
-            "service_type_display",
-            "description",
-            "base_duration_minutes")
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "service_type",
+        "get_service_type_display",
+        "base_duration_minutes",
+        "is_active",
+    )
+
+    list_filter = ("service_type", "is_active")
+    search_fields = ("title",)
