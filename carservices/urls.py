a6abbab562d9b96ea7ae9a2b5a69d5161e1af9b5
raminhrd <raminhr.work@gmail.com>
2026-01-30 16:36:12 +0330

    fix main urls 🐒

--- carservices/urls.py
+++ carservices/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('cars/', include("cars.urls")),
     path('services/', include("services.urls")),
     path('booking/', include("booking.urls")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
